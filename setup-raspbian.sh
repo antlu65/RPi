924af9d6c6b129aa9f9ec22e949fd178e54b59a8
antlu <antlu65@gmail.com>
2020-05-29 07:50:05 -0700

    asdf

--- setup-raspbian.sh
+++ setup-raspbian.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo -e "\n ***** [BEGIN] Setting up Raspberry Pi..."
-cd ~
+
+
 
 ### SETUP
 # hostname
@@ -16,54 +17,31 @@ echo -e "\n --- [TASK] Configuring hostname..."
 	sed "$newcommand" /etc/hosts > hosts
 	sudo mv -f hosts /etc/hosts
 echo -e " --- [OK]\n"
-
 # timezone, keyboard, ssh
 echo -e "\n --- [TASK] Configuring timezone..."
 	sudo rm -f /etc/localtime
 	sudo ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 echo -e " --- [OK]\n"
 echo -e "\n --- [TASK] Configuring keyboard..."
-	touch keyboard
-	echo -e "XKBMODEL=\"pc105\"\n" >> keyboard
-	echo -e "XKBLAYOUT=\"us\"" >> keyboard
-	echo -e "XKBVARIANT=\"\"" >> keyboard
-	echo -e "XKBOPTIONS=\"\"" >> keyboard
-	echo -e "\nBACKSPACE=\"guess\"" >> keyboard
-	sudo mv -f keyboard /etc/default/keyboard
+	kbconfig=./shared/keyboard
+	sudo mv -f $kbconfig /etc/default/keyboard
 echo -e " --- [OK]\n"
 echo -e "\n --- [TASK] Configuring ssh..."
 	sudo systemctl enable ssh
 	sudo systemctl start ssh
 echo -e " --- [OK]\n"
 
+
+
+# INSTALL
 # update/upgrade default software
-echo -e "\n --- [TASK] Updating and upgrading default software..."
+echo -e "\n --- [TASK] Updating default packages..."
 	sudo killall apt -q
 	sudo rm /var/lib/apt/lists/lock /var/cache/apt/archives/lock /var/lib/dpkg/lock* 2> /dev/null
 	sudo dpkg --configure -a
 	sudo apt update
 	sudo apt upgrade -y
 echo -e " --- [OK]\n"
-
-# networking/wifi
-echo -e "\n --- [TASK] Configuring networking and wifi..."
-	network="ATT3tf4ur4"
-	netpass="H3nrB1wan9n3t"
-	sudo apt install net-tools wireless-tools wpasupplicant -y
-	sudo ifconfig wlan0 up
-	wpa_passphrase "$network" "$netpass" | tee wpa_supplicant.conf &> /dev/null	
-	# echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" >> wpa_supplicant.conf
-	# echo "update_config=1" >> wpa_supplicant.conf
-	# echo "country=US" >> wpa_supplicant.conf
-	# echo -e "network=\n{\nssid=\"ATT3tf4ur4\"\npsk=\"H3nrB1wan9n3t\"\n}" >> wpa_supplicant.conf
-	sudo mv -f wpa_supplicant.conf /etc/wpa_supplicant.conf
-	sudo wpa_supplicant -B -c /etc/wpa_supplicant.conf -i wlan0
-	sudo dhclient wlan0
-	sudo cp /lib/systemd/system/wpa_supplicant.service /etc/systemd/system/wpa_supplicant.service
-echo -e " --- [OK]\n"
-
-
-
 # netcore
 echo -e "\n --- [TASK] Installing .NET Core runtimes..."
 	sudo apt install libunwind8 gettext -y
@@ -86,14 +64,23 @@ echo -e " --- [OK]\n"
 
 
 
-### CLEANUP
-echo -e "\n --- [TASK] Cleaning up..."
-# remove unnecessary packages.
-	sudo apt remove raspi-config -y &> /dev/null
-	sudo apt autoremove -y
+# networking/wifi
+echo -e "\n --- [TASK] Configuring networking..."
+	sudo apt install net-tools wireless-tools wpasupplicant -y
+	npconfig=./ubuntu/50-cloud-init.yaml
+	sudo mv -f $npconfig /etc/netplan/50-cloud-init.yaml
+	sudo netplan --debug generate
+	sudo netplan --debug apply
 echo -e " --- [OK]\n"
 
 
 
+### CLEANUP
+echo -e "\n --- [TASK] Cleaning up..."
+	sudo apt remove raspi-config
+	sudo apt autoremove -y
+echo -e " --- [OK]\n"
+echo -e " ----- [END] Raspberry Pi setup complete. Rebooting..."
+sleep 5
+sudo reboot
 ### END.
-echo -e " ----- [END] Raspberry Pi setup complete."
