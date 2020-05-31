5b6697f6c8440a703bbd30a632318b690d65b470
antlu65 <antlu65@gmail.com>
2020-05-31 02:42:28 -0700

    asdf

--- configKeyboard.sh
+++ configKeyboard.sh
@@ -8,41 +8,41 @@ scriptMinArgs=0
 scriptRequireRootUser=1
 beginScript() {
   echo -e "\n Executing $0 ..."
-  echo -e "------- [BEGIN] $scriptDescription ..."
+  echo -e " --- [BEGIN] $scriptDescription ..."
   
   # Ensure root user if needed.  
   if [[ "$scriptRequireRootUser" -eq 1 ]] && [[ "$EUID" -ne 0 ]]; then
-    echo " --- [Error] Must be root user."
+    echo " ----- [Error] Must be root user."
     exitScript -1
   # Ensure scriptDescription is not empty.
   elif [[ -n "$scriptDescription" ]]; then
-    echo " --- [InternalError] scriptDescription is empty."
+    echo " ----- [InternalError] scriptDescription is empty."
     exitScript -1
   # Ensure scriptMinArgs > 0.
   elif [[ "$scriptMinArgs" -lt 0 ]]; then
-    echo " --- [InternalError] scriptMinArgs < 0."
+    echo " ----- [InternalError] scriptMinArgs < 0."
     exitScript -1
   # Ensure scriptMaxArgs >= scriptMinArgs.
   elif [[ "$scriptMaxArgs" -lt "$scriptMinArgs" ]]; then
-    echo " --- [InternalError] scriptMaxArgs < scriptMinArgs."
+    echo " ----- [InternalError] scriptMaxArgs < scriptMinArgs."
     exitScript -1
   # Ensure number of args >= scriptMinArgs.
   elif [[ "$#" -lt "$scriptMinArgs" ]]; then
-    echo " --- [Error] Too few parameters ($#). Expected >= $scriptMinArgs."
+    echo " ----- [Error] Too few parameters ($#). Expected >= $scriptMinArgs."
     exitScript -1
   # Ensure args count <= scriptMaxArgs.
   elif [[ "$#" -gt "$scriptMaxArgs" ]]; then
-    echo " --- [Error] Too many parameters ($#). Expected <= $scriptMaxArgs."
+    echo " ----- [Error] Too many parameters ($#). Expected <= $scriptMaxArgs."
     exitScript -1
   fi
 }
 exitScript() { # $1 -> int for this script's exit code. 0 is success.
   if [[ "$1" -eq 0 ]]; then
-    echo -e " ------- [SUCCESS] $scriptDescription."
+    echo -e " --- [SUCCESS] $scriptDescription."
   else
-    echo -e " ------- [FAILED] $scriptDescription."
+    echo -e " --- [FAILED] $scriptDescription."
   fi
-  echo -e "\n Exited $0 with code $1."
+  echo -e "Exited $0 with code $1."
 }
 
 ### Script-specific Variables and Functions
