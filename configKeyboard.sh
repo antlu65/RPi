7706b837bb35df400b8589064957556b3a300957
antlu65 <antlu65@gmail.com>
2020-05-31 02:52:25 -0700

    Working on configKeyboard.sh

--- configKeyboard.sh
+++ configKeyboard.sh
@@ -10,33 +10,33 @@ beginScript() {
   echo -e "\nExecuting $0 ..."
   echo " [BEGIN] $scriptDescription"
   # Ensure root user if needed.  
-  if [[ "$scriptRequireRootUser" -eq 1 ]] && [[ "$EUID" -ne 0 ]]; then
+  if [ "$scriptRequireRootUser" -eq 1 ] && [ "$EUID" -ne 0 ]; then
     echo " ---- [Error] Must be root user."
     exitScript -1
   # Ensure scriptDescription is not empty.
-  elif [[ -n "$scriptDescription" ]]; then
+  elif [ -n "$scriptDescription" ]; then
     echo " ---- [InternalError] scriptDescription is empty."
     exitScript -1
   # Ensure scriptMinArgs > 0.
-  elif [[ "$scriptMinArgs" -lt 0 ]]; then
+  elif [ "$scriptMinArgs" -lt 0 ]; then
     echo " ---- [InternalError] scriptMinArgs < 0."
     exitScript -1
   # Ensure scriptMaxArgs >= scriptMinArgs.
-  elif [[ "$scriptMaxArgs" -lt "$scriptMinArgs" ]]; then
+  elif [ "$scriptMaxArgs" -lt "$scriptMinArgs" ]; then
     echo " ---- [InternalError] scriptMaxArgs < scriptMinArgs."
     exitScript -1
   # Ensure number of args >= scriptMinArgs.
-  elif [[ "$#" -lt "$scriptMinArgs" ]]; then
+  elif [ "$#" -lt "$scriptMinArgs" ]; then
     echo " ---- [Error] Too few parameters ($#). Expected >= $scriptMinArgs."
     exitScript -1
   # Ensure args count <= scriptMaxArgs.
-  elif [[ "$#" -gt "$scriptMaxArgs" ]]; then
+  elif [ "$#" -gt "$scriptMaxArgs" ]; then
     echo " ---- [Error] Too many parameters ($#). Expected <= $scriptMaxArgs."
     exitScript -1
   fi
 }
 exitScript() { # $1 -> int for this script's exit code. 0 is success.
-  if [[ "$1" -eq 0 ]]; then
+  if [ "$1" -eq 0 ]; then
     echo -e " [SUCCESS] $scriptDescription"
   else
     echo -e " [FAILED] $scriptDescription"
