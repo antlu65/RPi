023d30e4d9e704317df79cec0ff013e37231b6d9
antlu65 <antlu65.gmail.com>
2021-07-24 15:34:51 -0700

    Add default user to docker superuser group.

--- raspios.sh
+++ raspios.sh
@@ -159,6 +159,7 @@ echo -e " -*- Install Docker ... "
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo chmod +x get-docker.sh
 sudo ./get-docker.sh
+sudo usermod -aG docker pi
 sudo rm get-docker.sh
 
 # Cleanup.
